75b5b0cfb2bc88d99dab71fe83c11280b3d3af6d
Devans90 <devans8819@gmail.com>
2023-06-25 14:53:06 +0100

    Added a cheepskate bot and graphical summary

--- game/RunGame.py
+++ game/RunGame.py
@@ -1,10 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from forsale import *
 
 from Bots.GreedyBot import GreedyBot
 from Bots.RandomBot import RandomBot
+from Bots.CheapskateBot import CheapskateBot
+
+winner, winnertype, final_scores, df = play_game({GreedyBot: 1, CheapskateBot:1, RandomBot: 2})
 
 
-game = play_game({GreedyBot: 1, RandomBot: 3})
+
+# Create a unique combination of 'Player' and 'Index'
+df['PlayerIndex'] = df['Player'] + df['Player Index'].astype(str)
+
+for player_index in df['PlayerIndex'].unique():
+    subset = df[df['PlayerIndex'] == player_index]
+    plt.plot(subset['Round'], subset['Money'], label=player_index)
+
+plt.xlabel('Round')
+plt.ylabel('Money')
+plt.title('Money changes over Rounds by Player and Index')
+plt.legend(loc='upper left')
+
+plt.show()
 
 
 pass
